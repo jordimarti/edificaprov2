5bb3bd45c2d4959ee244f996b45f13dc695061ef
Jordi Martí Muñoz <jordimarti@me.com>
2017-04-21 13:08:52 +0200

    Activa la subscripció a mailchimp al registrar-se

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  #after_create :subscribe
+  after_create :subscribe
 
   validates :first_name, :presence => true
   validates :last_name, :presence => true
