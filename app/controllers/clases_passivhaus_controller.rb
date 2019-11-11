1d3e26bfee671d113d1ab975da5a902c13af749b
Jordi Martí Muñoz <jordimartimunoz@iMac-de-Jordi.local>
2019-11-11 13:32:26 +0100

    Afegeix usuari autoritzat per classes

--- app/controllers/clases_passivhaus_controller.rb
+++ app/controllers/clases_passivhaus_controller.rb
@@ -5,7 +5,7 @@ class ClasesPassivhausController < ApplicationController
 	private
   	def check_clases_access
 	    @this_user = User.find(current_user.id)
-	    if @this_user.id == 1 || @this_user.id == 2
+	    if @this_user.id == 1 || @this_user.id == 726
 	    	
 	    else
 	    	redirect_to clases_passivhaus_restricted_access_path
