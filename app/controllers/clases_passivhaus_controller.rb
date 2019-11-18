82f85998c5ed267f8dac8928ee65e15c58213f31
Jordi Martí Muñoz <jordimartimunoz@iMac-de-Jordi.local>
2019-11-18 15:07:49 +0100

    Afegeix index2 a classes passivhaus

--- app/controllers/clases_passivhaus_controller.rb
+++ app/controllers/clases_passivhaus_controller.rb
@@ -1,6 +1,6 @@
 class ClasesPassivhausController < ApplicationController
 	before_filter :authenticate_user!
-	before_filter :check_clases_access, only: [:index] 
+	before_filter :check_clases_access, only: [:index, :index2] 
 
 	private
   	def check_clases_access
