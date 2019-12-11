0262895d126eb1f1d3156e2a28561bfe09335b7d
Jordi Martí Muñoz <jordimartimunoz@iMac-de-Jordi.local>
2019-12-11 17:29:57 +0100

    Canvis en persones que poden accedir a les classes energiehaus

--- app/controllers/clases_passivhaus_controller.rb
+++ app/controllers/clases_passivhaus_controller.rb
@@ -5,7 +5,7 @@ class ClasesPassivhausController < ApplicationController
 	private
   	def check_clases_access
 	    @this_user = User.find(current_user.id)
-	    if @this_user.id == 1 || @this_user.id == 726 || @this_user.id == 728 || @this_user.id == 729 || @this_user.id == 730 || @this_user.id == 731 || @this_user.id == 732
+	    if @this_user.id == 1 || @this_user.id == 726
 	    	
 	    else
 	    	redirect_to clases_passivhaus_restricted_access_path
