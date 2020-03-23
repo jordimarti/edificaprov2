c011d75dd518b613b8f46fab6f625aa02f82498b
Jordi Martí Muñoz <jordimartimunoz@iMac-de-Jordi.local>
2020-03-23 23:21:07 +0100

    Afegeix alumne

--- app/controllers/clases_passivhaus_controller.rb
+++ app/controllers/clases_passivhaus_controller.rb
@@ -5,7 +5,7 @@ class ClasesPassivhausController < ApplicationController
 	private
   	def check_clases_access
 	    @this_user = User.find(current_user.id)
-	    if @this_user.id == 1 || @this_user.id == 726 || @this_user.id == 1527 || @this_user.id == 1531 || @this_user.id == 1532  || @this_user.id == 1533  || @this_user.id == 1534  || @this_user.id == 1535  || @this_user.id == 1536  || @this_user.id == 1537  || @this_user.id == 1538  || @this_user.id == 1539  || @this_user.id == 1540  || @this_user.id == 1541  || @this_user.id == 1542
+	    if @this_user.id == 1 || @this_user.id == 726 || @this_user.id == 1527 || @this_user.id == 1531 || @this_user.id == 1532  || @this_user.id == 1533  || @this_user.id == 1534  || @this_user.id == 1535  || @this_user.id == 1536  || @this_user.id == 1537  || @this_user.id == 1538  || @this_user.id == 1539  || @this_user.id == 1540  || @this_user.id == 1541  || @this_user.id == 1542 || @this_user.id == 1575
 	    	
 	    else
 	    	redirect_to clases_passivhaus_restricted_access_path
