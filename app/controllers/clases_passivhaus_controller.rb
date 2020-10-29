0d0b66cf763c5796bf06a6d484a6a0eb08997f56
Jordi Martí Muñoz <jordimartimunoz@iMac-de-Jordi.local>
2020-10-29 17:02:17 +0100

    Afegeix classes cpd i coavn

--- app/controllers/clases_passivhaus_controller.rb
+++ app/controllers/clases_passivhaus_controller.rb
@@ -1,6 +1,6 @@
 class ClasesPassivhausController < ApplicationController
 	before_filter :authenticate_user!
-	before_filter :check_clases_access, only: [:z6k9m, :pr6w2, :gc8x, :xk98e, :z48ym, :m3k21, :ap921, :pq4rt, :m5nto, :s34pgk, :s34mls, :d3sc4, :tr4d3s, :tr43x4, :d3s3x4, :la45i8m, :la45i8v, :sob73t, :mh93se, :cp82k7] 
+	before_filter :check_clases_access, only: [:z6k9m, :pr6w2, :gc8x, :xk98e, :z48ym, :m3k21, :ap921, :pq4rt, :m5nto, :s34pgk, :s34mls, :d3sc4, :tr4d3s, :tr43x4, :d3s3x4, :la45i8m, :la45i8v, :sob73t, :mh93se, :cp82k7, :coavn7x8] 
 
 	private
   	def check_clases_access
